2b3cdc52fcabeb4bb644ccd3e6d2185afa9c2155
Anton_P <84933235+Shpil5508@users.noreply.github.com>
2021-11-30 21:29:36 +0300

    lesson

---
+++ Variables.java
@@ -0,0 +1,9 @@
+class Variables {
+	public static void main(String[] args) {
+		int first = 10;
+		float second = 2;
+		second = first * second;
+		System.out.println(first);
+		System.out.println(second);
+	}
+}
