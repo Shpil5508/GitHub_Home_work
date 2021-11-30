581628d06aeb216d2c4913b8c735ef5d472041e0
Shpil5508 <84933235+Shpil5508@users.noreply.github.com>
2021-11-30 21:33:08 +0300

    Delete Variables.java

--- Variables.java
+++
@@ -1,9 +0,0 @@
-class Variables {
-	public static void main(String[] args) {
-		int first = 10;
-		float second = 2;
-		second = first * second;
-		System.out.println(first);
-		System.out.println(second);
-	}
-}
