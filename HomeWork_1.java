7c3c46acb2f7632777a3c5cb778c1026eb3c29cb
Anton_P <84933235+Shpil5508@users.noreply.github.com>
2021-12-12 16:21:39 +0300

    Update homework #1

--- HomeWork_1.java
+++ HomeWork_1.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class HomeWork_1 {
     public static void main(String[]args) {
         printThreeWords();
@@ -41,4 +42,49 @@ class HomeWork_1 {
             b = 4;
         System.out.println(a >= b ? "a >= b" : "a < b");
     }
+=======
+class HomeWork_1 {
+    public static void main(String[]args) {
+        printThreeWords();
+        checkSumSing();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    static void checkSumSing() {
+        int a = -1,
+            b = 1;
+        /* if (a + b >= 0) {
+        System.out.println("Сумма положительная");
+        } else {
+         System.out.println("Сумма отрицательная");
+        } */
+        System.out.println(a + b >= 0 ? "Сумма положительная" : "Сумма отрицательная"); //очень удобный тернарный оператор
+    }
+
+    static void printColor() {
+        int value = -5;
+        if (value <= 0) {
+        System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+        System.out.println("Желтый");
+        }
+        if (value > 100) {
+        System.out.println("Зелёный");
+        }
+    }
+
+    static void compareNumbers() {
+        int a = 4,
+            b = 4;
+        System.out.println(a >= b ? "a >= b" : "a < b");
+    }
+>>>>>>> Stashed changes
 }
