b519d55872b21373dfcb4ba04cbf2a6f0bd701a1
Anton_P <84933235+Shpil5508@users.noreply.github.com>
2021-12-15 19:16:01 +0300

    Update homework #2

--- HomeWork_2.java
+++ HomeWork_2.java
@@ -2,39 +2,37 @@
 * Java 1. Homework #2
 *
 * @author Paliev Anton
-* @version 12.12.2021
+* @version 14.12.2021
 */
 class HomeWork_2 {
     public static void main(String[] args) {
-        System.out.println(checkSum(5, 20));         // п.1 домашнего задания
-        System.out.println(positiveOrNegative(0));   // п.2 домашнего задания
-        System.out.println(whatNumber(1));           // п.3 домашнего задания
-        System.out.print(wordAndNumber("word ", 5)); // п.4 домашнего задания
+        System.out.println(checkSum(5, 10));         // п.1 ДЗ
+        positiveOrNegative(0);                       // п.2 ДЗ
+        System.out.println(whatNumber(0));           // п.3 ДЗ
+        wordAndNumber("happy ", 3);                  // п.4 ДЗ
+        isYearVisikosniy(400);
     }
     
-    static boolean checkSum (int a, int b) {   // п.1 домашнего задания
-        if (a + b >= 10 || a + b <= 20) {
-            return true;
-        }
-        return false;
+    static boolean checkSum (int a, int b) {         // п.1 ДЗ
+        return a + b >= 10  && a + b <= 20;
     }
     
-    static String positiveOrNegative (int a) {      // п.2 домашнего задания
-       return a >= 0 ? "Число положительное" : "Число отрицательное";
+    static void positiveOrNegative (int a) {         // п.2 ДЗ
+       System.out.println(a >= 0 ? "Число положительное" : "Число отрицательное");
        }
        
-    static boolean whatNumber (int a) {   // п.3 домашнего задания
-        if (a < 0) {
-            return true;
+    static boolean whatNumber (int a) {   // п.3 ДЗ
+        return a < 0;
         }
-        return false;
+
+    static void wordAndNumber (String w, int a)   {    // п.4 ДЗ
+        for (int j = 0; j < a; j++) {
+            System.out.print(w);
         }
-        
-    static String wordAndNumber (String a, int i)   {   // п.4 домашнего задания
-        String result = "";
-        for (int j = 0; j< i; j++) {
-            result = result + a;
-        }
-        return result;
+        System.out.println();
+    }
+    
+    static void isYearVisikosniy (int year) {
+        System.out.println(year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? "Год высокосный" : "Год невысокосный");
     }
 }
