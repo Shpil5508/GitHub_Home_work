e3098281b07b634faae9fdecaeb7363c50b44cb7
Shpil5508 <84933235+Shpil5508@users.noreply.github.com>
2022-05-26 22:44:05 +0300

    Delete ProductResult500.java

--- Homework #5/main/java/lesson_5/Homework_5/ProductResult500.java
+++
@@ -1,9 +0,0 @@
-package lesson_5.Homework_5;
-
-public class ProductResult500 {
-    private String timestamp;
-    private Integer status;
-    private String error;
-    private String message;
-    private String path;
-}
